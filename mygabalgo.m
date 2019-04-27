3d6b039b167becd5baac222f362407abe65b0246
kushWithoutWax <kush.lahiri@gmail.com>
2019-04-27 18:13:18 +0530

    just mygabalgo added

--- mygabalgo.m
+++ mygabalgo.m
@@ -1,13 +1,19 @@
-clear all;close all;
-
-
+%clear all;close all;
+% x = csvread('ymnst');
+ x=File;
+ S=x(10:4:end);
+ S=S/max(S);
+ [n,~]=size(S);
+ S=S';
+ 
+ L=round(x(2,1));   
 %%data Import section
 
 
-L=7;                                       %domain size
-n=2044;                                     %no of points
+                                    %domain size
+                                %no of points
 t2=linspace(0,L,n+1); t=t2(1:n);            %%time vector
-S=(0.3*sin(2*t)+0.4*sin(4*t)+tanh(t-1).^2 + 0.22* sin(3 * (t-4).^3)+0.5*exp(-(t-1).^2)+0.1*sin(4*t));                  %% signal
+%S=(0.3*sin(2*t)+0.4*sin(4*t)+tanh(t-1).^2 + 0.22* sin(3 * (t-4).^3)+0.5*exp(-(t-1).^2)+0.1*sin(4*t));                  %% signal
 
 k=(2*pi/L)*[0:n/2-1 -n/2:-1]; ks=fftshift(k);
 
@@ -15,7 +21,7 @@ St=fft(S);
 tres=0.1;
 iwindow_width=2;
 tslide=0:tres:L;
-locations=find(ks>=4 & ks<100 );
+locations=find(ks>=4 & ks<8 );
 array_hfc=[];
 for j=1:length(tslide)
 
@@ -30,12 +36,12 @@ total_hfc=trapz(array_spectra);         %%total high frequency components.
 array_hfc=[array_hfc,total_hfc];
 
 
-subplot(3,1,1), plot(t,S,'k',t,g,'r');
-subplot(3,1,2), plot(t,Sg,'k');
-subplot(3,1,3), plot(ks,spectra_windowed,'k');
-axis([-10 10 0 1])
-drawnow
-pause(0.1)
+% subplot(3,1,1), plot(t,S,'k',t,g,'r');
+% subplot(3,1,2), plot(t,Sg,'k');
+% subplot(3,1,3), plot(ks,spectra_windowed,'k');
+% axis([-10 10 0 1])
+% drawnow
+% pause(0.05)
 end
 
 
